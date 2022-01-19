4276562455494259a480fd6f9793ea971864a65e
mtisby <marieltisby1998@gmail.com>
2022-01-19 12:46:40 -0800

    check

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(session(sessionConfig))
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors({
-    origin: "https://modest-shirley-e38b5f.netlify.app/",
+    origin: "http://localhost:3000",
     credentials: true
 }))
 
