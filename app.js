9115df45fc0966b1b44ea9ba638062ed748ead00
mtisby <marieltisby1998@gmail.com>
2022-01-21 13:50:25 -0800

    deploy

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use(session(sessionConfig))
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://mtisby.github.io/cinema-save-client/",
     credentials: true
 }))
 
