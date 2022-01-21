e4ee3960a273bd8a9f4e2620bb53db302f2759d3
mtisby <marieltisby1998@gmail.com>
2022-01-21 13:49:22 -0800

    deploy

--- app.js
+++ app.js
@@ -118,20 +118,6 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-    res.header("Access-Control-Allow-Credentials", true);
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204);
-    }
-    next();
-  });
-
 app.post("/home", cors(), isLoggedIn, async (req, res) => {
     let userID = req.body.userid
     if(debugLvl1 === true){
